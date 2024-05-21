54e1424d53bdc60a38c24dfdbb576a3a9be657de
vpetel127 <vpetel127@cable.comcast.com>
2024-05-21 11:47:34 -0400

    Reports

---
+++ app/src/main/java/com/amarant/apps/budgetapp/ui/adapter/ReportsAdapter.kt
@@ -0,0 +1,55 @@
+package com.amarant.apps.budgetapp.ui.adapter
+
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.amarant.apps.budgetapp.R
+import com.amarant.apps.budgetapp.databinding.ItemBudgetBinding
+import com.amarant.apps.budgetapp.entities.Budget
+import com.amarant.apps.budgetapp.util.UtilityFunctions.dateMillisToString
+
+class ReportsAdapter : RecyclerView.Adapter<ReportsAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(ItemBudgetBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        with(holder) {
+            with(differ.currentList[position]) {
+                binding.budgetItemAmount.text = amount.toString()
+                binding.budgetItemDate.text = dateMillisToString(date.toLong())
+                binding.budgetItemPurpose.text = purpose
+                binding.budgetItemPurpose.tooltipText = purpose
+                if (creditOrDebit.equals("Credit")) {
+                    binding.budgetItemType.setImageResource(R.drawable.ic_credit)
+                } else {
+                    binding.budgetItemType.setImageResource(R.drawable.ic_debit)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    inner class MyViewHolder(val binding: ItemBudgetBinding): RecyclerView.ViewHolder(binding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Budget>() {
+        override fun areItemsTheSame(oldItem: Budget, newItem: Budget): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Budget, newItem: Budget): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+}
--- app/src/main/java/com/amarant/apps/budgetapp/ui/fragments/ReportsFragment.kt
+++ app/src/main/java/com/amarant/apps/budgetapp/ui/fragments/ReportsFragment.kt
@@ -5,13 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.amarant.apps.budgetapp.databinding.FragmentReportsBinding
+import com.amarant.apps.budgetapp.ui.adapter.ReportsAdapter
+import com.amarant.apps.budgetapp.ui.viewmodels.BudgetViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ReportsFragment : Fragment() {
 
     private var _binding: FragmentReportsBinding? = null
     private val binding: FragmentReportsBinding
         get() = _binding ?: throw RuntimeException("FragmentReportsBinding == null")
+
+    private val budgetViewModel: BudgetViewModel by viewModels()
+    private lateinit var reportsAdapter: ReportsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,8 +31,28 @@ class ReportsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeRecyclerView()
+        getAllEntries()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initializeRecyclerView() {
+        reportsAdapter = ReportsAdapter()
+        binding.rcvReports.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = reportsAdapter
+        }
+    }
+
+    private fun getAllEntries() {
+        budgetViewModel.allBudgetEntries.observe(viewLifecycleOwner) {
+            reportsAdapter.differ.submitList(it)
+        }
     }
 }
