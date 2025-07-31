0ddcb04a5e02e6b365be411e752584905a1c47b0
vpetel127 <vpetel127@cable.comcast.com>
2025-07-31 08:53:41 -0400

    Step Two Screen

--- app/src/main/java/com/amarant/apps/budgetapp/ui/fragments/OnboardingFragment.kt
+++ app/src/main/java/com/amarant/apps/budgetapp/ui/fragments/OnboardingFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.amarant.apps.budgetapp.databinding.FragmentOnboardingBinding
 import com.amarant.apps.budgetapp.ui.adapter.OnboardingAdapter
 import com.amarant.apps.budgetapp.ui.fragments.viewpager.StepOneFragment
@@ -39,12 +40,27 @@ class OnboardingFragment : Fragment() {
         val onboardingAdapter = OnboardingAdapter(requireActivity())
         binding.viewPagerOnboard.adapter = onboardingAdapter
         binding.viewPagerOnboard.isUserInputEnabled = false
+        binding.viewPagerOnboard.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                binding.btnPrevious.isEnabled = position > 0
+                binding.tvStep.text = "Step ${position + 1} of 5"
+                binding.pbarStep.setProgress(position + 1, true)
+            }
+        })
     }
 
     private fun setClickListeners() {
         binding.btnNext.setOnClickListener {
             val currentItem = binding.viewPagerOnboard.currentItem
-            binding.viewPagerOnboard.setCurrentItem(currentItem + 1, true)
+            if (currentItem < 4) {
+                binding.viewPagerOnboard.setCurrentItem(currentItem + 1, true)
+            }
+        }
+        binding.btnPrevious.setOnClickListener {
+            val currentItem = binding.viewPagerOnboard.currentItem
+            if (currentItem > 0) {
+                binding.viewPagerOnboard.setCurrentItem(currentItem - 1, true)
+            }
         }
     }
 }
