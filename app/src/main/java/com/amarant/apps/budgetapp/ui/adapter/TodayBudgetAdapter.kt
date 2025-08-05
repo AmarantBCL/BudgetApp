83f15fce7d4dbfdc90e68175a11f2fdb68d568dc
vpetel127 <vpetel127@cable.comcast.com>
2025-08-05 07:38:51 -0400

    Calendar Design

---
+++ app/src/main/java/com/amarant/apps/budgetapp/ui/adapter/TodayBudgetAdapter.kt
@@ -0,0 +1,62 @@
+package com.amarant.apps.budgetapp.ui.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.amarant.apps.budgetapp.R
+import com.amarant.apps.budgetapp.databinding.ListItemTodayEntryBinding
+import com.amarant.apps.budgetapp.entities.Budget
+
+class TodayBudgetAdapter : ListAdapter<Budget, TodayBudgetAdapter.TodayBudgetViewHolder>(TodayBudgetDiffItemCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodayBudgetViewHolder {
+        val binding = ListItemTodayEntryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TodayBudgetViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TodayBudgetViewHolder, position: Int) {
+        val item = getItem(position)
+        val context = holder.itemView.context
+        with(holder) {
+            binding.tvTitle.text = item.purpose
+            binding.tvCategory.text = item.category
+            binding.tvAmount.text = item.amount.toString()
+            val categoryImageResId = when (item.category) {
+                "Transportation" -> R.drawable.circle_transportation
+                "Restaurants" -> R.drawable.circle_cafe
+                "Groceries" -> R.drawable.circle_shopping
+                "Rent" -> R.drawable.circle_housing
+                "Health" -> R.drawable.circle_health
+                "Entertainment" -> R.drawable.circle_entertainment
+                "Cash" -> R.drawable.circle_income
+                else -> R.drawable.cat_unknown
+            }
+            binding.imgCategory.setImageResource(categoryImageResId)
+            if (item.creditOrDebit == "Credit") {
+//                binding.imgType.setImageResource(R.drawable.ic_credit)
+                binding.tvAmount.setTextColor(ContextCompat.getColor(context, R.color.positive_green))
+                binding.tvAmount.text = "+${item.amount}"
+            } else {
+//                binding.imgType.setImageResource(R.drawable.ic_debit)
+                binding.tvAmount.setTextColor(ContextCompat.getColor(context, R.color.negative_red))
+            }
+        }
+    }
+
+    class TodayBudgetViewHolder(val binding: ListItemTodayEntryBinding) : RecyclerView.ViewHolder(binding.root)
+
+    class TodayBudgetDiffItemCallback : DiffUtil.ItemCallback<Budget>() {
+
+        override fun areItemsTheSame(oldItem: Budget, newItem: Budget): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Budget, newItem: Budget): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/com/amarant/apps/budgetapp/ui/fragments/CalendarFragment.kt
+++ app/src/main/java/com/amarant/apps/budgetapp/ui/fragments/CalendarFragment.kt
@@ -6,18 +6,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import com.amarant.apps.budgetapp.R
 import com.amarant.apps.budgetapp.databinding.FragmentCalendarBinding
+import com.amarant.apps.budgetapp.entities.Budget
 import com.amarant.apps.budgetapp.entities.PiggyBank
+import com.amarant.apps.budgetapp.ui.MainActivity
+import com.amarant.apps.budgetapp.ui.adapter.TodayBudgetAdapter
 import com.amarant.apps.budgetapp.ui.viewmodels.PiggyBankViewModel
 import com.amarant.apps.budgetapp.ui.viewmodels.ProfileViewModel
 import com.amarant.apps.budgetapp.util.Constants.PREFERENCE_IS_PIN_ENTERED_KEY
 import com.amarant.apps.budgetapp.util.Constants.PREFERENCE_NAME
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale.filter
+import kotlin.math.absoluteValue
 
 @AndroidEntryPoint
 class CalendarFragment : Fragment() {
@@ -54,8 +62,43 @@ class CalendarFragment : Fragment() {
         activity?.title = "Calendar"
         binding.calendarView.setOnDateChangeListener { _, year, month, day ->
             val selectedDate = "$day/${month + 1}/$year"
-            val action = CalendarFragmentDirections.actionCalendarFragmentToBudgetEntryFragment(selectedDate)
-            findNavController().navigate(action)
+//            val action = CalendarFragmentDirections.actionCalendarFragmentToBudgetEntryFragment(selectedDate)
+//            findNavController().navigate(action)
+        }
+        val todayBudgetAdapter = TodayBudgetAdapter()
+        binding.recyclerEntries.adapter = todayBudgetAdapter
+        val entryOne = Budget(1, "2025-08-04", "Privatbank", -75f, "Morning coffee", "Debit", "Restaurants")
+        val entryTwo = Budget(2, "2025-08-04", "Privatbank", -208f, "Uber ride", "Debit", "Transportation")
+        val entryThree = Budget(3, "2025-08-04", "Privatbank", 25000f, "Salary", "Credit", "Cash")
+        val entryFour = Budget(4, "2025-08-04", "Privatbank", -855f, "Blood tests", "Debit", "Health")
+        val entryFive = Budget(5, "2025-08-04", "Privatbank", -9500f, "Apartment", "Debit", "Rent")
+        val entrySix = Budget(6, "2025-08-05", "Privatbank", -528f, "Supermarket", "Debit", "Groceries")
+        val entrySeven = Budget(7, "2025-08-05", "Privatbank", -791f, "Cyberpunk 2077", "Debit", "Entertainment")
+        val list = listOf<Budget>(entryOne, entryTwo, entryThree, entryFour, entryFive, entrySix, entrySeven)
+        todayBudgetAdapter.submitList(list)
+        binding.tvNumberOfEntries.text = "${list.size} entries"
+        if (list.isNotEmpty()) {
+            val totalIncome = list.filter { it.creditOrDebit == "Credit" }.sumOf { it.amount.toDouble() }
+            val totalExpenses = list.filter { it.creditOrDebit == "Debit" }.sumOf { it.amount.toDouble() }
+            val netIncome = totalIncome - totalExpenses.absoluteValue
+            binding.recyclerEntries.visibility = View.VISIBLE
+            binding.cardIncomeExpenses.visibility = View.VISIBLE
+            binding.tvEmptyEntries.visibility = View.GONE
+            binding.imgDollar.visibility = View.GONE
+            binding.tvIncome.text = "+$totalIncome"
+            binding.tvExpenses.text = totalExpenses.toString()
+            if (netIncome > 0) {
+                binding.tvNetIncome.setTextColor(ContextCompat.getColor(requireContext(), R.color.positive_green))
+                binding.tvNetIncome.text = "+$netIncome"
+            } else {
+                binding.tvNetIncome.setTextColor(ContextCompat.getColor(requireContext(), R.color.negative_red))
+                binding.tvNetIncome.text = netIncome.toString()
+            }
+        } else {
+            binding.recyclerEntries.visibility = View.GONE
+            binding.cardIncomeExpenses.visibility = View.GONE
+            binding.tvEmptyEntries.visibility = View.VISIBLE
+            binding.imgDollar.visibility = View.VISIBLE
         }
     }
 
