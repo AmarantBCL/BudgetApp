2733ddd948748804dd5cd97d97867bd30f7dfcb0
vpetel127 <vpetel127@cable.comcast.com>
2025-11-01 09:32:41 -0400

    Status for savings

--- app/src/main/java/com/amarant/apps/budgetapp/ui/adapter/SavingsAdapter.kt
+++ app/src/main/java/com/amarant/apps/budgetapp/ui/adapter/SavingsAdapter.kt
@@ -4,6 +4,7 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
@@ -14,8 +15,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.amarant.apps.budgetapp.R
 import com.amarant.apps.budgetapp.databinding.ListItemTargetBinding
 import com.amarant.apps.budgetapp.entities.Saving
+import com.amarant.apps.budgetapp.util.NumberUtils.formatCurrency
 import com.amarant.apps.budgetapp.util.NumberUtils.formatDecimal
 import com.amarant.apps.budgetapp.util.NumberUtils.formatNumberWithThousandsSeparator
+import androidx.core.graphics.toColorInt
+import kotlin.math.roundToInt
 
 class SavingsAdapter : ListAdapter<Saving, SavingsAdapter.SavingViewHolder>(SavingDiffItemCallback()) {
 
@@ -42,11 +46,38 @@ class SavingsAdapter : ListAdapter<Saving, SavingsAdapter.SavingViewHolder>(Savi
             val context = holder.itemView.context
             ImageViewCompat.setImageTintList(imgColorCircle, ColorStateList.valueOf(color))
             tvTargetName.text = item.title
-            chipCurrency.text = item.currency
-            tvProgress.text = context.getString(R.string.saving_progress_placeholder, currentSum, targetSum)
+            if (percent >= 100) {
+                val textColor = ContextCompat.getColorStateList(context, R.color.complete_text)
+                chipCurrency.setTextColor(textColor)
+                val bgColor = ContextCompat.getColorStateList(context, R.color.complete_background)
+                chipCurrency.chipBackgroundColor = bgColor
+                chipCurrency.text = context.getString(R.string.completed)
+                imgDaysLeft.visibility = View.GONE
+                tvDaysLeft.visibility = View.GONE
+                tvToGo.visibility = View.GONE
+            } else if (dueTo < 0) {
+                val textColor = ContextCompat.getColorStateList(context, R.color.overdue_text)
+                chipCurrency.setTextColor(textColor)
+                val bgColor = ContextCompat.getColorStateList(context, R.color.overdue_background)
+                chipCurrency.chipBackgroundColor = bgColor
+                chipCurrency.text = context.getString(R.string.overdue)
+                imgDaysLeft.visibility = View.VISIBLE
+                tvDaysLeft.visibility = View.VISIBLE
+                tvToGo.visibility = View.VISIBLE
+            } else {
+                val textColor = ContextCompat.getColorStateList(context, R.color.accent_purple)
+                chipCurrency.setTextColor(textColor)
+                val bgColor = ContextCompat.getColorStateList(context, R.color.background_dark_purple)
+                chipCurrency.chipBackgroundColor = bgColor
+                chipCurrency.text = context.getString(R.string.in_progress)
+                imgDaysLeft.visibility = View.VISIBLE
+                tvDaysLeft.visibility = View.VISIBLE
+                tvToGo.visibility = View.VISIBLE
+            }
+            tvProgress.text = context.getString(R.string.saving_progress_placeholder, currencySymbol.toString(), currentSum, targetSum)
             tvPercent.text = context.getString(R.string.percent_placeholder, formatDecimal(percent))
             tvToGo.text = context.getString(R.string.currency_and_amount_to_go_placeholder, currencySymbol, toGo)
-            tvDaysLeft.text = context.getString(R.string.days_left_placeholder, Math.round(dueTo).toString())
+            tvDaysLeft.text = context.getString(R.string.days_left_placeholder, dueTo.roundToInt().toString())
             pbProgress.progress = percent.toInt()
             cardSaving.setOnLongClickListener {
                 onSavingLongClickListener?.invoke(item)
--- app/src/main/java/com/amarant/apps/budgetapp/util/NumberUtils.kt
+++ app/src/main/java/com/amarant/apps/budgetapp/util/NumberUtils.kt
@@ -3,6 +3,7 @@ package com.amarant.apps.budgetapp.util
 import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.text.NumberFormat
+import java.util.Currency
 import java.util.Locale
 
 object NumberUtils {
@@ -16,5 +17,12 @@ object NumberUtils {
         val df = DecimalFormat("#.##")
         df.roundingMode = RoundingMode.HALF_UP
         return df.format(value)
+    }
+
+    fun formatCurrency(amount: Double, currencySymbol: String): String {
+        val formatter = NumberFormat.getCurrencyInstance(Locale.getDefault())
+        val currency = Currency.getInstance(currencySymbol)
+        formatter.currency = currency
+        return formatter.format(amount)
     }
 }
