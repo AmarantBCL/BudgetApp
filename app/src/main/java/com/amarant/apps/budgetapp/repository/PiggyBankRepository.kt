3d9737094ecdcb4789bdb0297fbdf5bf9a2d4f61
vpetel127 <vpetel127@cable.comcast.com>
2026-01-11 06:04:15 -0500

    Sorting by Completed savings goals

--- app/src/main/java/com/amarant/apps/budgetapp/repository/PiggyBankRepository.kt
+++ app/src/main/java/com/amarant/apps/budgetapp/repository/PiggyBankRepository.kt
@@ -1,5 +1,6 @@
 package com.amarant.apps.budgetapp.repository
 
+import androidx.lifecycle.map
 import com.amarant.apps.budgetapp.db.PiggyBankDao
 import com.amarant.apps.budgetapp.entities.PiggyBank
 import com.amarant.apps.budgetapp.entities.Saving
@@ -13,7 +14,14 @@ class PiggyBankRepository @Inject constructor(
 
     fun getPiggyBank() = piggyBankDao.getPiggyBank()
 
-    fun getAllSavings() = piggyBankDao.getAllSavings()
+    fun getAllSavings() = piggyBankDao.getAllSavings().map { savings ->
+        savings.sortedWith(
+            compareBy<Saving> {
+                val isAtBottom = it.target <= it.saved
+                if (isAtBottom) 0 else 1
+            }.thenBy { it.id }
+        )
+    }
 
     suspend fun addSaving(saving: Saving) = piggyBankDao.addSaving(saving)
 
