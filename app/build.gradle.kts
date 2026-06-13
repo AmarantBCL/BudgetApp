e0f0f600dcd3da41d887902fbb3c06c4963b1209
vpetel127 <vpetel127@cable.comcast.com>
2026-06-13 09:33:50 -0400

    Version 11 (1.15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.amarant.apps.budgetapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.14"
+        versionCode = 11
+        versionName = "1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
